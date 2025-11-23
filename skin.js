edf00ad79d0231cf220dcf51ec7cd030a020b3f8
randa-aji <randaaji@gmail.com>
2025-11-23 15:39:18 -0500

    Main

---
+++ skin.js
@@ -0,0 +1,153 @@
+// Garden Gnome Software - Skin
+// Pano2VR 7.1.8/20986
+// Filename: RA_v01.ggsk
+// Generated 2025-11-23T14:27:32
+
+function pano2vrSkin(player,base) {
+	var me=this;
+	var skin=this;
+	var flag=false;
+	var skinKeyPressedKey = 0;
+	var skinKeyPressedText = '';
+	this.player=player;
+	player.setApiVersion(7);
+	this.player.skinObj=this;
+	this.divSkin=player.divSkin;
+	this.ggUserdata=player.userdata;
+	this.lastSize={ w: -1,h: -1 };
+	var basePath="";
+	var cssPrefix="";
+	// auto detect base path
+	if (base=='?') {
+		var scripts = document.getElementsByTagName('script');
+		for(var i=0;i<scripts.length;i++) {
+			var src=scripts[i].src;
+			if (src.indexOf('skin.js')>=0) {
+				var p=src.lastIndexOf('/');
+				if (p>=0) {
+					basePath=src.substr(0,p+1);
+				}
+			}
+		}
+	} else
+	if (base) {
+		basePath=base;
+	}
+	this.elementMouseDown={};
+	this.elementMouseOver={};
+	var i;
+	var hs,el,els,elo,ela,elHorScrollFg,elHorScrollBg,elVertScrollFg,elVertScrollBg,elCornerBg;
+	var prefixes='Webkit,Moz,O,ms,Ms'.split(',');
+	for(var i=0;i<prefixes.length;i++) {
+		if (typeof document.body.style[prefixes[i] + 'Transform'] !== 'undefined') {
+			cssPrefix='-' + prefixes[i].toLowerCase() + '-';
+		}
+	}
+	
+	player.setMargins(0,0,0,0);
+	
+	this.updateSize=function(startElement) {
+		var stack=[];
+		stack.push(startElement);
+		while(stack.length>0) {
+			var e=stack.pop();
+			if (e.ggUpdatePosition) {
+				e.ggUpdatePosition();
+			}
+			if (e.hasChildNodes()) {
+				for(var i=0;i<e.childNodes.length;i++) {
+					stack.push(e.childNodes[i]);
+				}
+			}
+		}
+	}
+	
+	player.addListener('changenode', function() { me.ggUserdata=player.userdata; });
+	
+	var parameterToTransform=function(p) {
+		return p.def + 'translate(' + p.rx + 'px,' + p.ry + 'px) rotate(' + p.a + 'deg) scale(' + p.sx + ',' + p.sy + ')';
+	}
+	
+	this.findElements=function(id,regex) {
+		var r=[];
+		var stack=[];
+		var pat=new RegExp(id,'');
+		stack.push(me.divSkin);
+		while(stack.length>0) {
+			var e=stack.pop();
+			if (regex) {
+				if (pat.test(e.ggId)) r.push(e);
+			} else {
+				if (e.ggId==id) r.push(e);
+			}
+			if (e.hasChildNodes()) {
+				for(var i=0;i<e.childNodes.length;i++) {
+					stack.push(e.childNodes[i]);
+				}
+			}
+		}
+		return r;
+	}
+	
+	this._=function(text, params) {
+		return player._(text, params);
+	}
+	
+	this.languageChanged=function() {
+		var stack=[];
+		stack.push(me.divSkin);
+		while(stack.length>0) {
+			var e=stack.pop();
+			if (e.ggUpdateText) {
+				e.ggUpdateText();
+			}
+			if (e.ggUpdateAria) {
+				e.ggUpdateAria();
+			}
+			if (e.hasChildNodes()) {
+				for(var i=0;i<e.childNodes.length;i++) {
+					stack.push(e.childNodes[i]);
+				}
+			}
+		}
+	}
+	player.addListener('sizechanged', function () { me.updateSize(me.divSkin);});
+	player.addListener('languagechanged', this.languageChanged);
+	
+	this.addSkin=function() {
+		var hs='';
+		this.ggCurrentTime=new Date().getTime();
+	};
+	player.addListener('changenode', function() {
+		me.ggUserdata=player.userdata;
+	});
+	me.skinTimerEvent=function() {
+		if (player.isInVR()) return;
+		me.ggCurrentTime=new Date().getTime();
+	};
+	player.addListener('timer', me.skinTimerEvent);
+	me.addSkin();
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.appendChild(document.createTextNode('.ggskin { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 14px; line-height: normal; } .ggmarkdown p,.ggmarkdown h1,.ggmarkdown h2,.ggmarkdown h3,.ggmarkdown h4 { margin-top: 0px } .ggmarkdown { white-space:normal }'));
+	document.head.appendChild(style);
+	document.addEventListener('keyup', function(e) {
+		if (e.key === 'Enter' || e.key === ' ') {
+			let activeElement = document.activeElement;
+			if (activeElement.classList.contains('ggskin') && activeElement.onclick) activeElement.onclick();
+		}
+	});
+	document.addEventListener('keydown', function(e) {
+		if (e.key === 'Enter' || e.key === ' ') {
+			let activeElement = document.activeElement;
+			if (activeElement.classList.contains('ggskin') && activeElement.onmousedown) activeElement.onmousedown();
+		}
+	});
+	document.addEventListener('keyup', function(e) {
+		if (e.key === 'Enter' || e.key === ' ') {
+			let activeElement = document.activeElement;
+			if (activeElement.classList.contains('ggskin') && activeElement.onmouseup) activeElement.onmouseup();
+		}
+	});
+	me.skinTimerEvent();
+};
